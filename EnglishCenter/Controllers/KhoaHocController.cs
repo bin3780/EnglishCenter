aa831c156f0b9760bc65485f05c424b185426948
bin.pqh <binpro0203@gmail.com>
2022-03-31 12:54:05 +0700

    Create Dai Brand

---
+++ EnglishCenter/Controllers/KhoaHocController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EnglishCenter.Models;
+
+namespace EnglishCenter.Controllers
+{
+    public class KhoaHocController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: KhoaHoc
+        public ActionResult Index()
+        {
+            return View(db.KhoaHocs.ToList());
+        }
+
+        // GET: KhoaHoc/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhoaHoc khoaHoc = db.KhoaHocs.Find(id);
+            if (khoaHoc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khoaHoc);
+        }
+
+        // GET: KhoaHoc/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: KhoaHoc/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,id_gv,ngaybatdau,ngayketthuc,gia,id_level")] KhoaHoc khoaHoc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.KhoaHocs.Add(khoaHoc);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(khoaHoc);
+        }
+
+        // GET: KhoaHoc/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhoaHoc khoaHoc = db.KhoaHocs.Find(id);
+            if (khoaHoc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khoaHoc);
+        }
+
+        // POST: KhoaHoc/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,id_gv,ngaybatdau,ngayketthuc,gia,id_level")] KhoaHoc khoaHoc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(khoaHoc).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(khoaHoc);
+        }
+
+        // GET: KhoaHoc/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhoaHoc khoaHoc = db.KhoaHocs.Find(id);
+            if (khoaHoc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khoaHoc);
+        }
+
+        // POST: KhoaHoc/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            KhoaHoc khoaHoc = db.KhoaHocs.Find(id);
+            db.KhoaHocs.Remove(khoaHoc);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EnglishCenter/Models/Level.cs
@@ -0,0 +1,26 @@
+namespace EnglishCenter.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Level")]
+    public partial class Level
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Level()
+        {
+            KhoaHoc = new HashSet<KhoaHoc>();
+        }
+
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string name { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<KhoaHoc> KhoaHoc { get; set; }
+    }
+}
