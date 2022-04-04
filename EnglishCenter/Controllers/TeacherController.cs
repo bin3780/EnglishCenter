779a306efe7a668949d006693b60209c7205ca9d
bin.pqh <binpro0203@gmail.com>
2022-04-04 08:19:18 +0700

    1

---
+++ EnglishCenter/Controllers/TeacherController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnglishCenter.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        public ActionResult IndexTeacher()
+        {
+            return View();
+        }
+        public ActionResult GiaoAn()
+        {
+            return View();
+        }
+    }
+}
